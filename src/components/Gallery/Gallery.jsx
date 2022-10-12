5b3b0994b1eaed09834ba0331a475323a1fe7f36
Hameedarh <ajibadeolayimika@gmail.com>
2022-10-12 12:37:24 +0100

    slideshow

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -4,8 +4,13 @@ import ria1 from '../../assets/ria11.jpg'
 import ria2 from '../../assets/ria12.jpg'
 import ria3 from '../../assets/ria13.jpg'
 import ria4 from '../../assets/ria14.jpg'
-import ria5 from '../../assets/ria15.jpg'
+import ria5 from '../../assets/ria 15.jpg'
 import ria6 from '../../assets/ria16.jpg'
+import ria7 from '../../assets/ria17.jpg'
+import ria8 from '../../assets/ria18.jpg'
+import ria9 from '../../assets/ria19.jpg'
+import ria20 from '../../assets/ria20.jpg'
+
 
 function Gallery() {
   return (
@@ -36,10 +41,20 @@ function Gallery() {
                 <div class="slide-container">
                     <div class="slide-wrapper">
                         <img src={ria1} alt="gallery"/>
-                        <img src={ria2} alt="gallery"/>
+                        <img src={ria9} alt="gallery"/>
                         <img src={ria4} alt="gallery"/>
                         <img src={ria5} alt="gallery"/>
                         <img src={ria4} alt="gallery"/>
+                        <img src={ria20} alt="gallery"/>
+                    </div>
+                </div>
+                <div class="slide-container">
+                    <div class="slide-wrapper">
+                        <img src={ria7} alt="gallery"/>
+                        <img src={ria2} alt="gallery"/>
+                        <img src={ria3} alt="gallery"/>
+                        <img src={ria5} alt="gallery"/>
+                        <img src={ria8} alt="gallery"/>
                         <img src={ria6} alt="gallery"/>
                     </div>
                 </div>
