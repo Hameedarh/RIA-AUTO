08d3938be3ead1fe714290f27dd50d0bcd7587c1
Hameedarh <ajibadeolayimika@gmail.com>
2022-10-13 21:54:04 +0100

    sizing

--- src/components/service/Service.jsx
+++ src/components/service/Service.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Service.css'
 import paintgun from '../../assets/ria4.jpg'
+import sik from '../../assets/sikkens.png'
 
 function Service() {
   return (
@@ -15,6 +16,7 @@ function Service() {
                     <p className='ser-text-left'>
                     We use <span style={{color:"rgb(52,152,219) "}}>High-Quality</span> paints, automated baking oven, car waxing/buffing to ensure high quality of service is delivered to our prestigious clients/customers
                     </p>
+                    <img src={sik} alt="siklogo" width={120}/>
                 </div>
                 <div className='serdiv2'>
                     <img src={paintgun} alt="paintgun"/>
