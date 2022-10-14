2837332c4d190b932205ea7f4638c56f10e7a42d
Hameedarh <ajibadeolayimika@gmail.com>
2022-10-14 19:03:01 +0100

    slideshow sizing

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -17,9 +17,9 @@ import ria9 from '../../assets/ria19.jpg'
 function Gallery() {
     
 const fadeImages = [
-    "https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
-    "https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80",
-    "https://images.unsplash.com/photo-1536987333706-fc9adfb10d91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80"
+    "Pictures/ria11.jpg",
+    "Pictures/ria12.jpg",
+    "Pictures/ria13.jpg"
   ];
   return (
     <div className='gal-container' id='gallery'>
