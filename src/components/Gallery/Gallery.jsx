b81da91cae42411958ad1c062c283f08aab12318
Hameedarh <ajibadeolayimika@gmail.com>
2022-10-14 18:35:45 +0100

    react-slideshow

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Fade } from "react-slideshow-image";
+import "react-slideshow-image/dist/styles.css";
 import './Gallery.css'
 // import ria1 from '../../assets/ria11.jpg'
 import ria2 from '../../assets/ria12.jpg'
@@ -13,7 +15,12 @@ import ria9 from '../../assets/ria19.jpg'
 
 
 function Gallery() {
-
+    
+const fadeImages = [
+    "https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
+    "https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80",
+    "https://images.unsplash.com/photo-1536987333706-fc9adfb10d91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80"
+  ];
   return (
     <div className='gal-container' id='gallery'>
         <div className='gal-cont'>
@@ -45,7 +52,19 @@ function Gallery() {
                 </div>
             </div>
             <div className="slide-show">
-                
+                <div className="slide-container">
+                    <Fade>
+                        <div className="each-fade">
+                        <img src={fadeImages[0]}  alt="pic1"/>
+                        </div>
+                        <div className="each-fade">
+                        <img src={fadeImages[1]} alt="pic1"/>
+                        </div>
+                        <div className="each-fade">
+                        <img src={fadeImages[2]}  alt="pic1"/>
+                        </div>
+                    </Fade>
+                </div>
             </div>
         </div>
     </div>
